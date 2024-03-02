35cefe462269b0e632c0fa001a8a9e1119185d2e
michalrudy9 <michallrudy9@gmail.com>
2024-03-02 21:51:40 +0100

    Added some style for components

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import classes from "./NewTodo.module.css";
 
 const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoInputTextRef = useRef<HTMLInputElement>(null);
@@ -16,7 +17,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor="text">Todo text</label>
       <input type="text" id="text" ref={todoInputTextRef} />
       <button>Add Todo</button>
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Todo from "../models/Todo";
 import TodoItem from "./TodoItem";
+import classes from "./Todos.module.css";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item: Todo) => (
         <TodoItem key={item.id} text={item.text} />
       ))}
